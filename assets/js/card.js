8435d37f3a17762ccad7690d9391785b72c5ca4b
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-11-08 13:29:04 +0100

    feat: delete card

--- assets/js/card.js
+++ assets/js/card.js
@@ -75,7 +75,81 @@ const cardModule = {
   );  
 
     const cardEmplacement = goodList.querySelector('.panel-block');
+
+    const updateCardBtn = clone.querySelector('.box .is-narrow a:first-child');
+
+    clone.querySelector('.delete-card-btn').addEventListener('click', cardModule.deleteCard);
+
     cardEmplacement.appendChild(clone);
+
+    updateCardBtn.addEventListener('click', cardModule.showUpdateCardForm)
   },
+
+  showUpdateCardForm: (event) => {
+    event.preventDefault();
+    const divParent = event.target.closest('.columns');
+
+    const title = divParent.querySelector('.card-content');
+    const form = divParent.querySelector('form');
+
+    form.addEventListener('submit', cardModule.handleUpdateCard);
+
+    title.classList.add('is-hidden');
+    form.classList.remove('is-hidden');
+  },
+
+  handleUpdateCard: async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const jsonData = Object.fromEntries(formData.entries());
+    const stringify = JSON.stringify(jsonData);
+
+    try {
+      const response = await fetch(
+        `${utilModule.base_url}/cards/${formData.get('cardId')}`,
+        {
+          method: 'PUT',
+          body: stringify
+        }
+      );
+
+      const json = await response.json();
+      if (!response.ok) throw json;
+      
+      form.classList.add('is-hidden');
+      form.reset();
+
+      const cardContent = form.previousElementSibling;
+      cardContent.textContent = json.name;
+      cardContent.classList.remove('is-hidden');
+      cardContent.parentElement.parentElement.style.backgroundColor = json.color;
+    } catch (error) {
+      utilModule.notify(error.message, 5000, 'is-danger');
+      
+    }
+  },
+
+  deleteCard: async (event) => {
+    event.preventDefault();
+    
+    const cardId = event.target.closest('.box').dataset.cardId;
+    try {
+      const response = await fetch(`${utilModule.base_url}/cards/${cardId}`,
+      {
+        method: 'DELETE'
+      }
+    );
+
+    const json = await response.json();
+    if (!response.ok) throw json;
+
+    event.target.closest('.box').remove();
+      
+    } catch (error) {
+      utilModule.notify(error.message, 5000, 'is-danger');
+      
+    }
+  }
 
 }
