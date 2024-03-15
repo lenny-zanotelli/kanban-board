028543129f64543240ef74007cd96f9ab2754143
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-03-15 10:26:45 +0100

    build(back): delete adonis js because of problem with extension and peer dependecies with v6

--- backend/app/services/card_service.ts
+++
@@ -1,8 +0,0 @@
-import { CardFactory } from '#database/factories/card_factory'
-
-export default class CardService {
-  async card() {
-    const card = await CardFactory.createMany(5)
-    return card
-  }
-}
