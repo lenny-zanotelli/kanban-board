14587a734e381714c29f0340c7e5037b2dbc1738
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-03-22 13:37:59 +0100

    refactor(back): delete basic route hello world

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -10,11 +10,5 @@ const ListsController = () => import('#controllers/lists_controller')
 const CardsController = () => import('#controllers/cards_controller')
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
-
 router.resource('lists', ListsController).apiOnly()
 router.resource('cards', CardsController).apiOnly()
