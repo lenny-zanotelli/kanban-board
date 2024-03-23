2210d7f864ceccf6cfef62008237a8b2eb533153
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-03-23 10:42:05 +0100

    refactor(back): delete files from the other backend with express and typeorm

--- src/middlewares/notFound.ts
+++
@@ -1,5 +0,0 @@
-import { Response, Request } from "express";
-
-export function notFoundMiddleware(_req: Request, res: Response) {
-  res.status(404).send('Service does not exists\nCheck API docs for correct routes')
-};
