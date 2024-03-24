fe60561ba625b93e47ff5593c8dac36af41fcb0a
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-03-24 17:02:28 +0100

    feat(back):

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -7,9 +7,11 @@
 |
 */
 const CardsController = () => import('#controllers/cards_controller')
+const TagsController = () => import('#controllers/tags_controller')
 import router from '@adonisjs/core/services/router'
 
 router.resource('lists', '#controllers/lists_controller').apiOnly()
 router.get('lists/:id/cards', [CardsController, 'cardsInAList'])
 router.resource('cards', '#controllers/cards_controller').apiOnly()
 router.resource('tags', '#controllers/tags_controller').apiOnly()
+router.post('cards/:id/tags', [TagsController, 'associateTagToCard'])
